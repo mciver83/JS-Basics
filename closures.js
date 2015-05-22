c596de1b8c99adb771fb960efe8c1212f1e1e4d5
Mark McIver <markmciver83@gmail.com>
2015-05-22 16:58:00 -0600

    Answers to JS-Basics

--- closures.js
+++ closures.js
@@ -9,11 +9,11 @@ var outer = function(){
 //Above you're given a function that returns another function which has a closure over the name variable.
 //Invoke outer saving the return value into another variable called 'inner'.
 
-  //Code Here
+  var inner = outer();
 
 //Once you do that, invoke inner.
 
-  //Code Here
+  inner();
 
 
 
@@ -32,7 +32,9 @@ var callFriend = function(){
 //Above you're given a callFriend function that returns another function.
 //Do what you need to do in order to call your function and get 'Calling Jake at 435-215-9248' in your console.
 
-  //Code Here
+  var callJake = callFriend();
+
+  callJake('435-215-9248');
 
 
 
@@ -44,7 +46,13 @@ var callFriend = function(){
   Write a function called makeCounter that makes the following code work properly.
 */
 
-  //Code Here
+  var makeCounter = function(){
+      var number = 1;
+      return function(){
+          return number++;
+      }
+  }
+
   var count = makeCounter();
   count() // 1
   count() // 2
@@ -62,7 +70,18 @@ var callFriend = function(){
   (which invokes the original function that was passed in) that can only ever be executed once.
 */
 
-  //Code Here
+  var someFunction = function(anotherFunction){
+    var count = 0;
+    var newFunction = function(){
+      if (count < 1) {
+        count++;
+        return anotherFunction();
+      } else {
+        return 'you have already invoked the function';
+      }
+    }
+    return newFunction; 
+  }
 
 
 
@@ -71,10 +90,23 @@ var callFriend = function(){
 
 
 /*
-  Now, similar to the last problem, write a function called 'fnCounter' that accepts two parameters. The first parameter will be an anonymous function and the second parameter, 'N', will be a number. Now, in 'fnCounter', allow the anonymous funciton to be invoked 'N' number of times. After it's been invoked 'N' number of times, return 'STOP'.
+  Now, similar to the last problem, write a function called 'fnCounter' that accepts two parameters. 
+  The first parameter will be an anonymous function and the second parameter, 'N', will be a number. 
+  Now, in 'fnCounter', allow the anonymous funciton to be invoked 'N' number of times. 
+  After it's been invoked 'N' number of times, return 'STOP'.
 */
 
-
+var fnCounter = function(anonymousFunction, N){
+  var count = 0;
+  return function(){
+    if (count < N) {
+      count++;
+      return anonymousFunction();
+    } else {
+      return 'STOP'
+    }
+  }
+}
 
 //Next Problem
 
@@ -89,28 +121,44 @@ var callFriend = function(){
     }
   };
 
-  Above you have a function named counter. Examine the function (without running the code) then below write what you expect to happen when the funciton is invoked. *Hint: setTimeout calls a function or evaluates an expression after a specified number of milliseconds.
+  Above you have a function named counter. Examine the function (without running the code) then below write what you expect 
+  to happen when the funciton is invoked. *Hint: setTimeout calls a function or evaluates an expression after a specified number
+   of milliseconds.
 
-    //Answer Here
+    //I would expect the function to log the numbers 1 - 5 1 second apart
 
 
   Now, run the function in your console and note what happpens.
 
   Was your answer right or wrong?
 
-    //Answer Here
+    //My answer was wrong
 
 
   Fix the counter function so that it works the way you expect it to work. (logging 1 then 2 then 3, etc)
 */
 
-    //Code Here
+
+  var counter = function(){
+    for (i=1; i<=5; i++){
+      (function(x){setTimeout(function time(){
+        console.log(x);
+      }, x*1000);
+      })(i)
+    }
+  }
 
 
 
 //Next Problem
 
+var someFunction = function(num){
+  return function(){
+    return num;
+  }
+}
 
+var funcArray = [someFunction(0), someFunction(1), someFunction(2), someFunction(3), someFunction(4), someFunction(5)];
 
 /*
   Make the following code work
